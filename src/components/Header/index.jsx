104fefaa59f48758489a72fc0b1b3ef04bc8bb25
Aurélien Home <aurelien.lorence@gmail.com>
2023-01-25 13:41:40 +0100

    Ajout liens sur logos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import './index.scss';
 
 import logo from './logo.svg';
+import {Link} from "react-router-dom";
 
 function Header(props) {
     return (
         <header className={"header"}>
-            <img src={logo} alt={"Logo Kasa"} />
+            <Link to={"/"}>
+                <img src={logo} alt={"Logo Kasa"} />
+            </Link>
             <nav>
                 <ul>
                     {props.children}
